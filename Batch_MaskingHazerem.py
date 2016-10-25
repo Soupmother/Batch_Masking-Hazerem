e6f17f199127e68cc02d0da915704f0f9737f671
Soupmother <t-kp@hotmail.co.uk>
2016-10-25 15:29:58 +0800

    Testing hazerem
    using this earlier block of code I had made to test applying batch processing to the hazerem script I will try to figure out why the new 'match' list and maskfili input is stopping hazerem from functioning.

--- Batch_MaskingHazerem.py
+++ Batch_MaskingHazerem.py
@@ -6,88 +6,31 @@ from pci.pansharp2 import pansharp2
 from pci.exceptions import *
 
 # Set the working directory
-working_dir = r'D:\PCI_Python\GLSvsL5'
-
+working_dir = r'D:\PCI_Python\GLSvsL5\GLS2010'
 
 # Set output directories
-masks_output_dir = os.path.join(working_dir, '~Masks')
-if not os.path.isdir(masks_output_dir):
-    os.mkdir(masks_output_dir)
-    
 haze_output_dir = os.path.join(working_dir, '~Haze Removed')
 if not os.path.isdir(haze_output_dir):
-    os.mkdir(haze_output_dir)    
-
-final_output_dir = os.path.join(working_dir, '~Final Output')    
-if not os.path.isdir(final_output_dir):
-    os.mkdir(final_output_dir)
-
+    os.mkdir(haze_output_dir)
     
-# Create 'input_files' list and set search criteria for 'os.walk'
+# Set input file criteria - note that this list doens not contain a _mask.pix image previously created by 'masking', so
+# is is not a useful opperation for haze removal, but just a test to see where hazerem is going wrong
 input_files = []
 
-for root, dirs, files in os.walk(working_dir):
-    for inFile in fnmatch.filter(files, "*_MTL.txt"):   
-        input_files.append(os.path.join(root, inFile))
-
-
-# Create cloud, water and haze masks for Landsat 7 images
+for r, d, f in os.walk(working_dir):
+    for inFile in fnmatch.filter(f, '*_MTL.txt'):
+        input_files.append(os.path.join(r, inFile))
+        
+# Remove the haze from both the multispectral and panchromatic bands
 for image in input_files:
-    try:
-        masking(fili='-'.join([image, 'MS']),
-                hazecov=[15],
-                clthresh=[18,22,1],
-                filo=os.path.join(masks_output_dir, '_mask.'.join([os.path.basename(image).split('_MTL')[0], 'pix'])))
-    except PCIException, e:
-        print e
-    except Exception, e:
-        print e       
-
-for image in input_files:
-    try:
-        masking(fili='-'.join([image, 'MS']),
-                asensor="Landsat-5 TM",
-                hazecov=[15],
-                clthresh=[18,22,1],
-                filo=os.path.join(masks_output_dir, '_mask.'.join([os.path.basename(image).split('_MTL')[0], 'pix'])))
-    except PCIException, e:
-        print e
-    except Exception, e:
-        print e        
-
-# Create new 'matches' list including '*.pix' output file from 'masking'        
-images = ['*_MTL.txt', '*_mask.pix']
-matches = []
-
-for root, dirnames, filenames in os.walk("working_dir"):
-    for extensions in images:
-        for filename in fnmatch.filter(filenames, extensions):
-            matches.append(os.path.join(root, filename))        
-
-# Run haze removal on Landsat 7 images using files from 'matches' list
-for image in matches:
     try:
         hazerem(fili='-'.join([image, 'MS']),
                 fili_pan='-'.join([image, 'PAN']),
-                maskfili='_mask.'.join([os.path.basename(image).split('_MTL')[0], 'pix']),
-                asensor="Landsat-7 ETM+",
                 hazecov=[15],
                 filo=os.path.join(haze_output_dir, '_haze_ms.'.join([os.path.basename(image).split('_MTL')[0], 'pix'])),
                 filo_pan=os.path.join(haze_output_dir, '_haze_pan.'.join([os.path.basename(image).split('_MTL')[0], 'pix'])))
     except PCIException, e:
         print e
     except Exception, e:
-        print e       
+        print e
 
-# Runs haze removal on the Landsat 5 images
-for image in matches:
-    try:
-        hazerem(fili='-'.join([image, 'MS']),
-                maskfili='_mask.'.join([os.path.basename(image).split('_MTL')[0], 'pix']),
-                asensor="Landsat-5 TM",
-                hazecov=[15],
-                filo=os.path.join(haze_output_dir, '_haze_ms.'.join([os.path.basename(image).split('_MTL')[0], 'pix'])))
-    except PCIException, e:
-        print e
-    except Exception, e:
-        print e
